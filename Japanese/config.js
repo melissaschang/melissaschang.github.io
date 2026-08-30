634d1eff745ef30b54ff4d7c04469992c27b7388
melissaschang <40808810+melissaschang@users.noreply.github.com>
2026-08-31 05:13:19 +0800

    Update config.js

--- Japanese/config.js
+++ Japanese/config.js
@@ -2,5 +2,7 @@
 // Use your Supabase PROJECT URL and PUBLISHABLE (anon) key.
 // NEVER put a service_role/secret key in this file.
 
-export const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
-export const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
+export const SUPABASE_URL = "https://ptijxhbuiwmvojjvcxxv.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_h6oH9ZKTvnnsQhM-ECNDJA_QqsPnx2l";
+//NEXT_PUBLIC_SUPABASE_URL=https://ptijxhbuiwmvojjvcxxv.supabase.co
+//NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_h6oH9ZKTvnnsQhM-ECNDJA_QqsPnx2l
